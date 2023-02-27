d3a2b063497e9a4bab9661ca2534f2eadd2c40fd
harusann <gp22a074@oecu.jp>
2023-02-28 01:36:55 +0900

    htmlファイルの追加

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
 const PORT = 3000;
-const STATIC_DIR = 'static'
+const STATIC_DIR = 'static';
+
+// MySQLを使うためのインストールしたmysqlを呼び出す
+
+//定数connectionに、createConnectionを使って接続するデータベースの情報を格納します。
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'sasaki8610',　//実際にはパスワードが設定されています。
+  port : 3306,
+  database: 'js_test'
+});
 
 app.use(express.static(STATIC_DIR));//STATIC_DIRディレクトリを静的ファイルの置き場に使用
 
