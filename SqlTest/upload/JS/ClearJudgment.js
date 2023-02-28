e15ef3d16149ff1e770d40d24910a2207de79985
shiryu1202 <106821745+shiryu1202@users.noreply.github.com>
2023-02-28 13:48:44 +0900

    hiyokoマーカーを認識すると、mysql上のデータベースに変更を加える機能を追加

---
+++ SqlTest/upload/JS/ClearJudgment.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent('box3', {
+    init: function()
+    {
+
+      var marker = this.el;
+
+      marker.addEventListener('markerFound', function () {
+        console.log("Desk_Chairを発見した");
+        location.href = '/clearWindow';
+      });
+
+      marker.addEventListener('markerLost', function () {
+        console.log("Desk_Chairを見失った");
+      });
+    }
+});
