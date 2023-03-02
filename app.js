7d814201f783ca90f5d693c809ecb0a474f0307d
harusann <gp22a074@oecu.jp>
2023-03-02 20:06:47 +0900

    データベース関連のファイルの削除

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
 
 const PORT = 3000;
 const STATIC_DIR = 'static';
-
-// MySQLを使うためのインストールしたmysqlを呼び出す
-
-//定数connectionに、createConnectionを使って接続するデータベースの情報を格納します。
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'sasaki8610',　//実際にはパスワードが設定されています。
-  port : 3306,
-  database: 'js_test'
-});
+app.set('view engine', 'ejs');
 
 app.use(express.static(STATIC_DIR));//STATIC_DIRディレクトリを静的ファイルの置き場に使用
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/templates/index.html');
-});
+    res.render(__dirname + '/views/ejs/index.ejs');
+})
 
 var listener = app.listen(PORT, () => {
     console.log('PORT : ' + listener.address().port);
--- static/js/ARinit.js
+++ static/js/ARinit.js
@@ -1,8 +1,16 @@
+class FoundMarker {
+  founded =  new Boolean();
+  id = new String();
+}
+
 AFRAME.registerComponent('marker', {
     init: function () {
 
       const $markers = document.querySelectorAll('a-marker')//a-markerタグのDOM要素を引用
       $markers.forEach((marker) => {//a-markerタグの要素それぞれにイベントを登録
+      const $answer = document.querySelector('answer');
+      
+      console.log($answer)
         // マーカーを検出した際のイベント
         marker.addEventListener('markerFound', function () {
             var markerId = marker.id;
@@ -18,3 +26,7 @@ AFRAME.registerComponent('marker', {
       
     }
 });
+
+MarkerSense = (markerId) => {
+
+}
