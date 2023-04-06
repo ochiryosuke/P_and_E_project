004364ac4349b96ebe5b68bf5500e7b259e6e26e
shiryu1202 <106821745+shiryu1202@users.noreply.github.com>
2023-04-06 10:59:43 +0900

    ARマーカーの回転を検知

--- SqlTest/upload/JS/BoxSearcher.js
+++ SqlTest/upload/JS/BoxSearcher.js
@@ -1,17 +1,34 @@
 AFRAME.registerComponent('box1', {
-    init: function()
-    {
+  init: function()
+  {
 
-      var marker = this.el;
+    var marker = this.el;
 
-      marker.addEventListener('markerFound', function () {
-        console.log("box1を発見した");
-        $('#box2').attr('src', './image/maru.png');
-      });
+    marker.addEventListener('markerFound', function () {
+      console.log("box1を発見した");
+      $('#box2').attr('src', './image/maru.png');
+    });
 
-      marker.addEventListener('markerLost', function () {
-        console.log("box1を見失った");
-        $('#box2').attr('src', './image/batu.png');
-      });
-    }
+
+
+
+    marker.addEventListener('markerLost', function () {
+      console.log("box1を見失った");
+      $('#box2').attr('src', './image/batu.png');
+    });
+  },
+
+  tick: function(delta) {
+
+    var marker = this.el;
+
+    const rot = marker.getAttribute('rotation');
+    console.log("x："+rot.x+"y："+rot.y+"z："+rot.z);
+},
+
+tick: function (time, timeDelta)
+  {
+  console.log("回転中");
+      this.el.object3D.rotation.z += 0.01
+  }
 });
