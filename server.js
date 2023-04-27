e806d6e05621fbab51688b7801bbd9cb421d8aa5
hu-san1008 <gp21a137@oecu.jp>
2023-04-28 00:54:52 +0900

    localサーバー上でプレイ出来るようにしました

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+
 const PORT = 5000;
-const html = require("fs").readFileSync("./index.html")
 
+app.use(express.static("public"));
 
-//webサーバーを作ろう
-const server = http.createServer((req, res) => {
-    //ブラウザからアクセスが来た時の処理
-    res.writeHead(200, { "Content-Type": "text/html"});
-    res.write(html);
-    res.end();
-});
-
-server.listen(PORT, () => {
-    console.log("server running!");
-});
+app.listen(PORT, () => console.log("サーバーが起動しました。"));
