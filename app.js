060da8de22ca93a0e5ee806f3385b666836872ae
harusann <gp22a074@oecu.jp>
2023-06-15 16:29:40 +0900

    remove console log

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const STATIC_DIR = 'static';
 app.use(express.static(STATIC_DIR));//STATIC_DIRディレクトリを静的ファイルの置き場に使用
 
 app.get('/', (req, res) => {
-    console.log(__dirname + '/views/index.html')
     res.sendFile(__dirname + '/views/index.html');
 })
 
